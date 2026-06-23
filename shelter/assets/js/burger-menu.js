c175af41c232a1f05e8c0de0086455d738c5e550
greysar <srdurov@gmail.com>
2026-06-24 00:20:44 +0500

    feat: add initial js for burger button

---
+++ shelter/assets/js/burger-menu.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const body = document.querySelector('body');
+const nav = document.querySelector('.nav');
+const navList = document.querySelector('.nav__list');
+
+let checkResolution = () => {
+    
+}
